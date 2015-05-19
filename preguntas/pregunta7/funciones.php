a51dc1bda5d8b263d0d9c0874f11bd61d54ecbf9
chelo1988 <marcelothuglife@hotmail.com>
2015-05-19 18:41:57 -0500

    preguntas

---
+++ preguntas/pregunta7/funciones.php
@@ -0,0 +1,37 @@
+<?php
+paises_capitales = array(
+    'Argentina' => 'Buenos Aires',
+    'Bolivia' => 'la paz',
+    'Brasil' => 'Brasilia',
+    'Chile' => 'Santiago',
+    'Colombia' => 'Bogota'
+);
+
+?>
+
+
+function generar_html()
+
+{
+
+<html>
+<head>
+<title> Paises Y Capitales </title>
+ </head>
+ <body>  
+
+  <table border =1>
+  <tr><td>Tabla</td>
+  </tr> 
+      <td>Paises</td>
+      <td> Capitales</td>
+      </tr><td>Fila 2 Columna 1</td>
+      <td> Fila 2 Columna 2</td>
+      </tr>
+      </table>
+
+</body>
+ </html>
+
+
+}
---
+++ preguntas/pregunta7/index.php
@@ -0,0 +1,14 @@
+<?php
+include('funciones.php');
+?>
+<html>
+<head>
+  <title>Paises y Capitales</title>
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+<?php
+  generar_html();
+?>
+</body>
+</html>
